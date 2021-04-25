20d998e5da0b2658ed07ac879f0ed7960f5eb0fa
Yue <dong.0.yue+git@gmail.com>
2021-04-25 18:35:13 +0900

    add character level judgement

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
 function fillActions() {
     let job = $("#job_selected").val()
     let combo = $("#combo_selected").val()
-    let l = Object.keys(actions[job][combo]).length
     if (combo == '重置') {
         fillReset()
-    } else if (l == 2){
+        return
+    }
+    let l = Object.keys(actions[job][combo]['actions']).length
+    if (l == 2){
         $("#special_parser").addClass("d-none")
         $("#normal_parser").removeClass("d-none")
         // $("#action_3").addClass("d-none")
@@ -28,10 +30,10 @@ function fillNormal(job, combo) {
         $(`#action_${i}`).addClass("d-none")
         $(`#label_action_${i}`).addClass("d-none")
     }
-    for (let i in actions[job][combo]) {
+    for (let i in actions[job][combo]['actions']) {
         $(`#${i}`).removeClass("d-none")
         $(`#label_${i}`).removeClass("d-none")
-        $(`#${i}`).val(actions[job][combo][i])
+        $(`#${i}`).val(actions[job][combo]['actions'][i])
     }
     $("#combo").val(combo)
 }   
@@ -57,7 +59,7 @@ function parseCombo() {
     } else if (jQuery.inArray(combo, changeWithCD) != -1) {
         var path = 'templates/changewithcd.xml'
     } else {
-        let l = Object.keys(actions[job][combo]).length
+        let l = Object.keys(actions[job][combo]['actions']).length
         var path = `templates/combo${l}stage.xml`
     }
     parseNormal(job, combo, path)
@@ -67,15 +69,22 @@ function parseCombo() {
 async function parseNormal(job, combo, path) {
     let template = await fetch(path).then(response => response.text())
     var settings = {}
-    for (let i in actions[job][combo]) {
-        // settings[i] = actions[job][combo][i]
+    for (let i in actions[job][combo]['actions']) {
+        // settings[i] = actions[job][combo]['actions'][i]
         settings[i] = $(`#${i}`).val()
         let s = combo + '-' + settings[i]
         settings[`id_${i}`] = $.md5(s)
     }
-    settings['combo'] = $("#combo").val()
-    settings['id_combo'] = $.md5(combo)
+    if (actions[job][combo]['key_levels']){
+        for (let j in actions[job][combo]['key_levels']) {
+            // console.log(actions[job][combo]['key_levels'][j])
+            settings[`key_level_${j}`] = actions[job][combo]['key_levels'][j]
+            // console.log(settings)
+        }
+    }
     settings['position'] = $("#pos_1").val() + ' ' + $("#pos_2").val()
+    settings['combo'] = $("#combo").val() + ' ' + settings['position']
+    settings['id_combo'] = $.md5($("#job").val()+settings['combo'])
     if ($('#is_corss_true').prop('checked')) {
         settings['is_cross'] = 'c'
     } else {
